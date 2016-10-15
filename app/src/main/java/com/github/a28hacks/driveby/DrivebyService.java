3a008ab089fc552cf9eae196bea7f8bfa19cb16c
Stefan Oberdörfer <stefan.oberdoerfer@outlook.com>
2016-10-15 16:23:21 +0200

    Sticky service for location updates

---
+++ app/src/main/java/com/github/a28hacks/driveby/DrivebyService.java
@@ -0,0 +1,83 @@
+package com.github.a28hacks.driveby;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.location.LocationManager;
+import android.os.IBinder;
+import android.util.Log;
+
+/**
+ * Created by stefan on 15.10.16.
+ */
+
+public class DrivebyService extends Service {
+    private static final String TAG = "DrivebyService";
+    private LocationManager mLocationManager = null;
+    private static final int LOCATION_INTERVAL = 1000;
+    private static final float LOCATION_DISTANCE = 10f;
+
+
+    DbLocationListener[] mLocationListeners = new DbLocationListener[]{
+            new DbLocationListener(LocationManager.GPS_PROVIDER),
+            new DbLocationListener(LocationManager.NETWORK_PROVIDER)
+    };
+
+    @Override
+    public IBinder onBind(Intent arg0) {
+        Log.e(TAG, "onStartCommand");
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.e(TAG, "onStartCommand");
+        super.onStartCommand(intent, flags, startId);
+        return START_STICKY;
+    }
+
+    @Override
+    public void onCreate() {
+        Log.e(TAG, "onCreate");
+        initializeLocationManager();
+        try {
+            mLocationManager.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER, LOCATION_INTERVAL, LOCATION_DISTANCE,
+                    mLocationListeners[1]);
+        } catch (java.lang.SecurityException ex) {
+            Log.i(TAG, "fail to request location update, ignore", ex);
+        } catch (IllegalArgumentException ex) {
+            Log.d(TAG, "network provider does not exist, " + ex.getMessage());
+        }
+        try {
+            mLocationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER, LOCATION_INTERVAL, LOCATION_DISTANCE,
+                    mLocationListeners[0]);
+        } catch (java.lang.SecurityException ex) {
+            Log.i(TAG, "fail to request location update, ignore", ex);
+        } catch (IllegalArgumentException ex) {
+            Log.d(TAG, "gps provider does not exist " + ex.getMessage());
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.e(TAG, "onDestroy");
+        super.onDestroy();
+        if (mLocationManager != null) {
+            for (int i = 0; i < mLocationListeners.length; i++) {
+                try {
+                    mLocationManager.removeUpdates(mLocationListeners[i]);
+                } catch (SecurityException ex) {
+                    Log.i(TAG, "fail to remove location listners, ignore", ex);
+                }
+            }
+        }
+    }
+
+    private void initializeLocationManager() {
+        if (mLocationManager == null) {
+            mLocationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+        }
+    }
+}
--- app/src/main/java/com/github/a28hacks/driveby/MainActivity.java
+++ app/src/main/java/com/github/a28hacks/driveby/MainActivity.java
@@ -1,13 +1,95 @@
 package com.github.a28hacks.driveby;
 
+import android.Manifest;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_REQ_CODE = 1337;
+
+    @BindView(R.id.btn_start_services)
+    protected Button toggleServicesBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        checkPermissions();
+
+        if(isMyServiceRunning(DrivebyService.class)) {
+            toggleServicesBtn.setText("Stop Driveby");
+        }
+
+        toggleServicesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleServices();
+            }
+        });
+
+    }
+
+
+    void toggleServices(){
+        if(isMyServiceRunning(DrivebyService.class)){
+            stopService(new Intent(this, DrivebyService.class));
+            toggleServicesBtn.setText("Start Driveby");
+        } else {
+            startService(new Intent(this, DrivebyService.class));
+            toggleServicesBtn.setText("Stop Driveby");
+        }
+    }
+
+
+    private boolean isMyServiceRunning(Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void checkPermissions() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            //always ask for permission
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSION_REQ_CODE);
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if(requestCode == PERMISSION_REQ_CODE) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startService(new Intent(this, DrivebyService.class));
+            } else {
+                //boo
+                finish();
+            }
+        }
     }
 }
